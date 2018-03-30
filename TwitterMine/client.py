3b3148650c10192a8b2813eb806c4ddf71e65286
jonahar <jonahar@gmail.com>
2018-03-30 17:13:19 +0300

    client progress

--- TwitterMine/client.py
+++ TwitterMine/client.py
@@ -4,34 +4,43 @@ import re
 import argparse
 from sys import stdin
 
-interactive_prompt = """TwitterMine Client
-Enter a command to execute. Command structure is one of:
+# description and help
+interactive_prompt = """\
+--TwitterMine Client--
+Enter a command to execute. To quit enter 'exit'.
+"""
+commands_help = """\
+A valid command should be in one of the following formats:
     mine <resource> of <screen_name> [limit]
-    listen to user [screen_name]
-    listen to keywords [comma,separated,keywords]
+    listen to user <screen_name>
+    listen to keywords <comma,separated,keywords>
 
-where resource is one of: 'details', 'friends', 'followers', 'tweets', 'likes'
-Enter 'exit' to quit
+where <resource> is one of: 'details', 'friends', 'followers', 'tweets', 'likes'
 """
 
-server_host_port = ''
+server_host_port = ''  # to be initialized according to config
 headers = {'content-type': 'application/json'}
+
+# valid commands regex
+mine_command_regex = re.compile(
+    '\s*mine\s+(details|friends|followers|tweets|likes)\s+of\s+\w+(\s+\d*)?\s*')
+listen_command_regex = re.compile(
+    '\s*listen\s+to\s+(user\s+\w+)|(keywords?\s+\w+(,\w+)*)\s*')
 
 
 def parse_args():
     """
     parse and return the program arguments
     """
-    parser = argparse.ArgumentParser(prog='TwitterMiner Client',
-                                     usage='client [OPTIONS]',
-                                     description='Twitter data extraction service (client)')
+    parser = argparse.ArgumentParser(description='TwitterMine client',
+                                     epilog=commands_help)
     parser.add_argument('-c', '--conf',
                         help='configurations file for the client (default \'client.conf\')',
                         required=False, type=str, default='client.conf')
 
     # exactly one of -s and -i is allowed
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-s', help='script file with list of commands')
+    group.add_argument('-s', help='script file with list of commands', metavar='script')
     group.add_argument('-i', help='start the client in interactive mode', action='store_true')
     return parser.parse_args()
 
@@ -89,70 +98,73 @@ LISTEN_TYPE_IDX = 2
 LISTEN_PARAM_IDX = 3
 
 
-class Client:
-    def __init__(self, script=None):
-        self.script = script
-        self.mine_command_regex = re.compile(
-            '\s*mine\s+(details|friends|followers|tweets|likes)\s+of\s+\w+(\s+\d*)?\s*')
-        self.listen_command_regex = re.compile(
-            '\s*listen\s+to\s+(user\s+\w+)|(keywords?\s+\w+(,\w+)*)\s*')
-
-    def execute_single_command(self, command):
-        """
-        :param command: string. the command to execute
-        """
-        if self.mine_command_regex.fullmatch(command):
-            tokens = command.split()
-            resource = tokens[RESOURCE_IDX]
-            screen_name = tokens[SCR_NAME_IDX]
-            if len(tokens) > LIMIT_IDX:
-                limit = int(tokens[LIMIT_IDX])
-            else:
-                limit = 0
-            if resource == 'details':
-                request_user_details(screen_name)
-            elif resource == 'friends':
-                request_friends_ids(screen_name, limit)
-            elif resource == 'followers':
-                request_followers_ids(screen_name, limit)
-            elif resource == 'tweets':
-                request_tweets(screen_name, limit)
-            elif resource == 'likes':
-                request_likes(screen_name, limit)
-
-        elif self.listen_command_regex.fullmatch(command):
-            tokens = command.split()
-            listen_type = tokens[LISTEN_TYPE_IDX]
-            param = tokens[LISTEN_PARAM_IDX]
-            if listen_type == 'user':
-                raise NotImplemented('listen to user not implemented')
-            elif listen_type in ['keyword', 'keywords']:
-                raise NotImplemented('listen to keywords not implemented')
+def execute_single_command(command):
+    """
+    :param command: string. the command to execute
+    """
+    if mine_command_regex.fullmatch(command):
+        tokens = command.split()
+        resource = tokens[RESOURCE_IDX]
+        screen_name = tokens[SCR_NAME_IDX]
+        if len(tokens) > LIMIT_IDX:
+            limit = int(tokens[LIMIT_IDX])
         else:
-            print('Invalid command:', command)
+            limit = 0
+        if resource == 'details':
+            request_user_details(screen_name)
+        elif resource == 'friends':
+            request_friends_ids(screen_name, limit)
+        elif resource == 'followers':
+            request_followers_ids(screen_name, limit)
+        elif resource == 'tweets':
+            request_tweets(screen_name, limit)
+        elif resource == 'likes':
+            request_likes(screen_name, limit)
 
-    def execute_commands(self, stream):
-        """
-        :param stream: file-like stream  of commands, separated with newlines
-        """
-        for line in stream:
-            line = line[:-1]  # remove newline character
-            if line == '':
-                continue
-            elif line == 'exit':
-                break
-            else:
-                self.execute_single_command(line)
+    elif listen_command_regex.fullmatch(command):
+        tokens = command.split()
+        listen_type = tokens[LISTEN_TYPE_IDX]
+        param = tokens[LISTEN_PARAM_IDX]
+        if listen_type == 'user':
+            raise NotImplemented('listen to user not implemented')
+        elif listen_type in ['keyword', 'keywords']:
+            raise NotImplemented('listen to keywords not implemented')
+    else:
+        print('Invalid command:', command)
 
-    def interactive_mode(self):
-        print(interactive_prompt)
-        stream = stdin
-        self.execute_commands(stream)
 
-    def script_mode(self):
-        stream = open(self.script)
-        self.execute_commands(stream)
-        stream.close()
+def execute_commands(stream):
+    """
+    :param stream: file-like stream  of commands, separated with newlines
+    """
+    for line in stream:
+        line = line[:-1]  # remove newline character
+        if line == '':
+            continue
+        elif line == 'exit':
+            break
+        else:
+            execute_single_command(line)
+
+
+def interactive_mode():
+    """
+    run the client in interactive mode
+    """
+    print(interactive_prompt)
+    print(commands_help)
+    stream = stdin
+    execute_commands(stream)
+
+
+def script_mode(script):
+    """
+    run the client in script mode
+    :param script: path to commands file
+    """
+    stream = open(script)
+    execute_commands(stream)
+    stream.close()
 
 
 if __name__ == '__main__':
@@ -166,6 +178,6 @@ if __name__ == '__main__':
               'Please check that the daemon is running and the host:port are correct')
         exit(1)
     if args.i:
-        Client().interactive_mode()
+        interactive_mode()
     else:  # script mode
-        Client(args.s).script_mode()
+        script_mode(args.s)
