b95a7efd1ffd1c69efc185f0bf8348916d6d7008
jonahar <jonahar@gmail.com>
2018-04-06 13:16:30 +0300

    instruct to set the config file

--- TwitterMine/test_toolbox.py
+++ TwitterMine/test_toolbox.py
@@ -19,14 +19,15 @@ logging.getLogger('requests_oauthlib').setLevel(logging.WARNING)
 logging.getLogger('urllib3').setLevel(logging.WARNING)
 logging.getLogger('werkzeug').setLevel(logging.WARNING)
 
+# TODO change this variable to point to your config file
+CONF_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../my_config/server.conf')
+
 
 def get_config():
     """
     :return: the dictionary with the different configurations
     """
-    conf_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                             '../my_config/server.conf')
-    with open(conf_file) as f:
+    with open(CONF_FILE) as f:
         config = json.load(f)
     return config
 
@@ -48,8 +49,3 @@ def get_writer():
 def get_miner():
     config = get_config()
     return Miner(config['consumer_key'], config['consumer_secret'], config['data_dir'])
-
-
-writer = get_writer()
-miner = get_miner()
-api = get_api()
