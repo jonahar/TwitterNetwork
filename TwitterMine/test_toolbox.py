ee8a3aa18e5d08036e6de3d091575328af416fb9
jonahar <jonahar@gmail.com>
2018-04-14 16:56:14 +0300

    added server object to toolbox & add submodule to python path for dev

--- TwitterMine/test_toolbox.py
+++ TwitterMine/test_toolbox.py
@@ -1,12 +1,20 @@
 import sys
+
+# for importing TwitterAPI in the dev folder structure
+try:
+    sys.path.insert(0, 'TwitterAPI_submodule')
+except Exception as e:
+    pass
+
 import os
 import logging
 import json
+from TwitterMine.server import Server
 from TwitterAPI import TwitterAPI
 from TwitterMine.data_writer import DataWriter as DW
 from TwitterMine.miner import Miner
 
-logging.basicConfig(filename='tester.log', level=logging.DEBUG,
+logging.basicConfig(filename='server_dev.log', level=logging.DEBUG,
                     format='%(asctime)s: %(levelname)s: %(filename)s: %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
                     filemode='a')
@@ -20,14 +28,17 @@ logging.getLogger('urllib3').setLevel(logging.WARNING)
 logging.getLogger('werkzeug').setLevel(logging.WARNING)
 
 # TODO change this variable to point to your config file
-CONF_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../my_config/server.conf')
+SERVER_CONF_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                '../my_config/server.conf')
+CLIENT_CONF_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                '../my_config/client.conf')
 
 
 def get_config():
     """
     :return: the dictionary with the different configurations
     """
-    with open(CONF_FILE) as f:
+    with open(SERVER_CONF_FILE) as f:
         config = json.load(f)
     return config
 
@@ -48,4 +59,12 @@ def get_writer():
 
 def get_miner():
     config = get_config()
-    return Miner(config['consumer_key'], config['consumer_secret'], config['data_dir'])
+    return Miner(config['consumer_key'], config['consumer_secret'],
+                 config['access_token_key'], config['access_token_secret'], config['data_dir'])
+
+
+def get_server():
+    config = get_config()
+    return Server(config['consumer_key'], config['consumer_secret'],
+                  config['access_token_key'], config['access_token_secret'],
+                  config['data_dir'], config['port'])
