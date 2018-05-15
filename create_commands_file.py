eee0d40b6fa4d5b8ead56b4177651bcf5b64b169
jonahar <jonahar@gmail.com>
2018-05-15 17:52:34 +0300

    create commands script

--- create_commands_file.py
+++ create_commands_file.py
@@ -1,12 +1,12 @@
 NEWLINE = '\n'
 
-results_file = 'netanyahu-nuclear-iran.results.sorted'
-commands_file = 'client-commands'
+results_file = 'bitcoin|btc.results.sorted'
+commands_file = 'bitcoin-tweets-client-commands'
 
 with open(results_file) as r, open(commands_file, mode='w') as c:
     for line in r:
         screen_name = line.split(';')[1]
-        c.write('mine details of {0}'.format(screen_name))
+        c.write('mine tweets of {0} 200'.format(screen_name))
         c.write(NEWLINE)
-        c.write('mine friends of {0} 5000'.format(screen_name))
-        c.write(NEWLINE)
+        #c.write('mine friends of {0} 5000'.format(screen_name))
+        #c.write(NEWLINE)
