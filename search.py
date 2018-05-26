e65a289001fbbb825f5616a13c7a80f289828580
jonahar <jonahar@gmail.com>
2018-05-26 14:41:57 +0300

    identify comments and retweets

--- search.py
+++ search.py
@@ -6,6 +6,24 @@ miner = test_toolbox.get_miner()
 api = miner.api
 
 
+def is_comment_retweet(t):
+    """
+    :param: t: tweet object (dictionary)
+    :return: if the given tweet is a comment or a retweet of another tweet, return the screen_name
+             of the author of the original tweet. otherwise, return None.
+    """
+    if t['in_reply_to_screen_name'] is not None:
+        # this is a direct comment
+        return t['in_reply_to_screen_name']
+    if 'retweeted_status' in t:
+        # this is a retweet with extra text (modification of the original tweet)
+        return t['retweeted_status']['user']['screen_name']
+    if 'quoted_status' in t:
+        # this is an unmodified retweet
+        return t['quoted_status']['user']['screen_name']
+    return None
+
+
 def search_comment_to_user(author_scr_name):
     """
     generator of tweets wwhich are replies to tweets by the given user
