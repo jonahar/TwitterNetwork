b257d2f1d5c2adce8061a9cd83ad67528c75c14c
jonahar <jonahar@gmail.com>
2018-05-26 14:23:40 +0300

    search

--- search.py
+++ search.py
@@ -2,17 +2,38 @@ import json
 from TwitterMine import test_toolbox
 from TwitterAPI.TwitterPager import TwitterPager
 
-# search for important tweets (filter by number of retweets and likes), and write authors to file
-
-NEWLINE = '\n'
-
 miner = test_toolbox.get_miner()
 api = miner.api
-# SEARCH_TERM = 'iota (crypto OR cryptocurrency) -xrp'
-SEARCH_TERM = 'bitcoin OR btc'
-MIN_RETWEETS = 400
-MIN_LIKES = 0
-MAX_TWEETS = 10000
+
+
+def search_comment_to_user(author_scr_name):
+    """
+    generator of tweets wwhich are replies to tweets by the given user
+    :param author_scr_name:
+    """
+    q = 'to:' + author_scr_name
+    r = TwitterPager(api, 'search/tweets', params={'q': q,
+                                                   'tweet_mode': 'extended',
+                                                   'count': 100})
+    for t in r.get_iterator():
+        yield t
+
+
+def search_comments_to_tweet(tweet_id, author_scr_name):
+    """
+    generator of tweets which are replies for the given tweet
+
+    :param tweet_id: tweet id of the tweet to which comments are searched
+    :param author_scr_name: screen name of the tweet's author
+    """
+    q = 'to:' + author_scr_name
+    r = TwitterPager(api, 'search/tweets', params={'q': q,
+                                                   'sinceId': tweet_id,
+                                                   'tweet_mode': 'extended',
+                                                   'count': 100})
+    for t in r.get_iterator():
+        if t['in_reply_to_status_id'] == tweet_id:
+            yield t
 
 
 def search_tweets(term, min_retweets=0, min_likes=0):
@@ -61,17 +82,39 @@ def user_repr(user):
     return '{0};{1};{2}'.format(user['id'], user['screen_name'], user['name'])
 
 
-out_filename = SEARCH_TERM + '.results'
+def download_users(term, max_tweets, min_retweets=0, min_likes=0):
+    """
+    download users whose tweets come up in the search of the given term
+    (in case of a retweet also download original author)
 
-with open(out_filename, mode='w') as outfile:
+    :param term: the search term
+    :param max_tweets: maximum number of tweets to search (positive integer)
+    :param min_retweets: consider only tweets that have at least this amount of retweets
+    :param min_likes: consider only tweets that have at least this amount of likes
+    :return: list of strings. each string is in the format "<user_id>;<screen_name>;<name>"
+    """
+    users = []
     count = 0
-    for t in search_tweets(SEARCH_TERM, MIN_RETWEETS, MIN_LIKES):
-        outfile.write(user_repr(t['user']))
-        outfile.write(NEWLINE)
+    for t in search_tweets(term, min_retweets, min_likes):
+        users.append(user_repr(t['user']))
+        # also get details of the original tweet author, if exist
         if 'retweeted_status' in t:
-            # also write details of the original tweet author
-            outfile.write(user_repr(t['retweeted_status']['user']))
-            outfile.write(NEWLINE)
+            users.append(user_repr(t['retweeted_status']['user']))
+        elif 'quoted_status' in t:
+            users.append(user_repr(t['quoted_status']['user']))
         count += 1
-        if count > MAX_TWEETS:
+        if count > max_tweets:
             break
+    return users
+
+
+MIN_RETWEETS = 200
+MIN_LIKES = 0
+MAX_TWEETS = 100000
+
+if __name__ == '__main__':
+    term = '(israel OR zionist) (hamas OR gaza OR palestine) (fence OR crimes OR terror)'
+    users = download_users(term, MAX_TWEETS, MIN_RETWEETS, MIN_LIKES)
+    out_filename = 'israel-gaza.results'
+    with open(out_filename, mode='a+') as f:
+        f.writelines(users)
