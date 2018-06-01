38e04bd321c7286e9921235d151d6409d678ffde
jonahar <jonahar@gmail.com>
2018-06-01 18:21:42 +0300

    create commands script

--- create_commands_file.py
+++ create_commands_file.py
@@ -1,10 +1,7 @@
-NEWLINE = '\n'
+results_file = '/cs/usr/jonahar/PycharmProjects/TwitterMine/israel-palestine-search-results'
+commands_file = '/cs/usr/jonahar/PycharmProjects/TwitterMine/israel-palestine-client-commands'
 
-results_file = '/cs/usr/jonahar/PycharmProjects/TwitterMine/israel-gaza.results.sorted'
-commands_file = '/cs/usr/jonahar/PycharmProjects/TwitterMine/israel-gaza-client-commands'
-
-with open(results_file) as r, open(commands_file, mode='w') as c:
+with open(results_file, encoding='utf-8') as r, open(commands_file, mode='w', encoding='utf-8') as c:
     for line in r:
-        screen_name = line.split(';')[1]
-        c.write('mine details of {0}'.format(screen_name))
-        c.write(NEWLINE)
+        screen_name = line[:-1].split(';')[1]
+        c.write('mine neighbors of {0} 2000\n'.format(screen_name))
