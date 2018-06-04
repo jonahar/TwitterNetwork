26e1b22e9355986184f99901d4058e7831dbbda5
jonahar <jonahar@gmail.com>
2018-06-04 09:34:45 +0300

    class prediction

--- predict_class.py
+++ predict_class.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 from collections import Counter
 
 from TwitterMine import test_toolbox
@@ -11,21 +12,9 @@ with open(network_users_filename) as f:
     id_to_name = json.load(f)['id_to_name']
 
 with open(modularity_classes_file) as f:
-    name_to_class = json.load(f)
-
-
-def prediction_data_mine(screen_name, timeline_limit=100000, friends_limit=500000,
-                         followers_limit=500000):
-    """
-    mine relevant information to predict the class of the given user
-    :return:
-    """
-    miner = test_toolbox.get_miner()
-    miner.run()
-    miner.produce_job('neighbors', {'screen_name': screen_name, 'limit': timeline_limit})
-    miner.produce_job('friends', {'screen_name': screen_name, 'limit': friends_limit})
-    miner.produce_job('followers', {'screen_name': screen_name, 'limit': followers_limit})
-    miner.stop()
+    o = json.load(f)
+    name_to_class = o['name_to_class']
+    class_label = o['class_label']
 
 
 def get_predictions_from_file(counter, file, line_type='id'):
@@ -40,14 +29,21 @@ def get_predictions_from_file(counter, file, line_type='id'):
     if os.path.exists(file):
         with open(file) as f:
             for line in f:
-                if type == 'id':
-                    name = id_to_name[int(line[:-1])]
-                    if name in name_to_class:
+                if line_type == 'id':
+                    id = int(line[:-1])
+                    if id in id_to_name:
+                        name = id_to_name[id]
                         counter[name_to_class[name]] += 1
-                elif type == 'name':
+                elif line_type == 'name':
                     name = line[:-1]
                     if name in name_to_class:
                         counter[name_to_class[name]] += 1
+    return counter
+
+
+timeline_limit = 100000
+friends_limit = 25000
+followers_limit = 25000
 
 
 def predict_class(screen_name):
@@ -63,16 +59,30 @@ def predict_class(screen_name):
     followers_file = os.path.join(data_dir, screen_name, 'followers')
     neighbors_file = os.path.join(data_dir, screen_name, 'neighbors')
 
-    # counter = get_predictions_from_file(counter, friends_file, 'id')
-    # counter = get_predictions_from_file(counter, followers_file, 'id')
-    # counter = get_predictions_from_file(counter, neighbors_file, 'name')
+    # mine relevant information
+    miner = test_toolbox.get_miner()
+    miner.run()
+    if not os.path.exists(friends_file):
+        miner.produce_job('friends_ids', {'screen_name': screen_name, 'limit': friends_limit})
+    if not os.path.exists(followers_file):
+        miner.produce_job('followers_ids', {'screen_name': screen_name, 'limit': followers_limit})
+    if not os.path.exists(neighbors_file):
+        miner.produce_job('neighbors', {'screen_name': screen_name, 'limit': timeline_limit})
+    miner.stop()
+
+    counter = get_predictions_from_file(counter, friends_file, 'id')
+    counter = get_predictions_from_file(counter, followers_file, 'id')
+    counter = get_predictions_from_file(counter, neighbors_file, 'name')
 
     total_weight = sum(counter.values())
     probabilities = {k: v / total_weight for k, v in counter.items()}
     return probabilities
 
 
-scr_name = 'realDonaldTrump'
-prediction_data_mine(scr_name)
-prob = predict_class(scr_name)
-print(json.dumps(prob, indent=4))
+if __name__ == '__main__':
+    scr_name = sys.argv[1]
+    prob = predict_class(scr_name)
+    for c in prob:
+        if c in class_label:
+            prob[c] = '{0} ({1})'.format(prob[c], class_label[c])
+    print(json.dumps(prob, indent=4))
