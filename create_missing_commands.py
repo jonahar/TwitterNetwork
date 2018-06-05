e5ffbe5c9a0a733f2c6366a50f497c9500b04e0d
jonahar <jonahar@gmail.com>
2018-06-05 20:03:48 +0300

    updated missing commands script

--- create_missing_commands.py
+++ create_missing_commands.py
@@ -1,19 +1,29 @@
 import os
 
-
-data_dir = '/cs/labs/avivz/jonahar/Twitter/israel_gaza_data_dir'
-in_file = '/cs/usr/jonahar/PycharmProjects/TwitterMine/israel-palestine-search-results'
-commands_file = '/cs/usr/jonahar/PycharmProjects/TwitterMine/israel-palestine-missing-commands'
+data_dir = '/cs/labs/avivz/jonahar/Twitter/cryptocurrency_data_dir'
+in_file = '/cs/usr/jonahar/PycharmProjects/TwitterMine/cryptocurrency-search-results'
+commands_file = '/cs/usr/jonahar/PycharmProjects/TwitterMine/cryptocurrency-missing-commands'
 
 neighbors_commands = 0
 details_commands = 0
 
-with open(in_file) as ifs, open(commands_file, mode='w') as c:
+users = set()
+with open(in_file) as ifs:
     for line in ifs:
-        scr_name = line[:-1].split(';')[1]
+        tokens = line[:-1].split(';')
+        scr_name = tokens[1]
+        users.add(scr_name)
+        original_scr_name = tokens[2]
+        if original_scr_name:
+            users.add(original_scr_name)
+
+
+
+with  open(commands_file, mode='w') as c:
+    for scr_name in users:
         user_dir = os.path.join(data_dir, scr_name)
         if not os.path.isdir(user_dir):
-            c.write('mine neighbors of {0} 2000\n'.format(scr_name))
+            c.write('mine neighbors of {0} 2500\n'.format(scr_name))
             neighbors_commands += 1
             details_commands += 1
         else:
